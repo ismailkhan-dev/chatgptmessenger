7bbd59148a00ee947e86e773847796cd1be242ab
viz1er <viz1er@yopmail.com>
2023-10-06 16:21:29 -0400

    add session providers for login

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import SideBar from "@/components/SideBar";
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Session, getServerSession } from "next-auth";
+import { SessionProvider } from "@/components/SessionProvider";
+import { GET } from "@/app/api/auth/[...nextauth]/route";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,22 +13,35 @@ export const metadata: Metadata = {
     description: "Using Next.js and OpenAI API",
 };
 
-export default function RootLayout({
+export default async function RootLayout({
     children,
 }: {
     children: React.ReactNode;
 }) {
+    const session: Session | null = await getServerSession(GET);
     return (
         <html lang="en">
             <head />
-            <body className={inter.className}>
-                <div className="flex">
-                    {/* Sidebar */}
-                    <SideBar />
-                    {/* ClientProvider - Notification */}
 
-                    <div className="bg-[#343541] flex-1"> {children}</div>
-                </div>
+            <body className={inter.className}>
+                <SessionProvider session={session}>
+                    {!session ? (
+                        <Login />
+                    ) : (
+                        <div className="flex">
+                            {/* Sidebar */}
+                            <div className="bg-[#202123] max-w-xs h-screen overflow-y-auto md:min-w-[20rem]">
+                                <SideBar />
+                            </div>
+                            {/* ClientProvider - Notification */}
+
+                            <div className="bg-[#343541] flex-1">
+                                {" "}
+                                {children}
+                            </div>
+                        </div>
+                    )}
+                </SessionProvider>
             </body>
         </html>
     );
--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -1,13 +1,12 @@
 import React from "react";
+import NewChat from "./NewChat";
 
 function SideBar() {
     return (
         <div className="p-2 flex flex-col h-screen ">
             <div className="flex-1">
                 {/* NewChat */}
-                {/* <NewChat /> */}
-                <div>{/* Model Selection */}</div>
-
+                <NewChat />\<div>{/* Model Selection */}</div>
                 {/* Map through the ChatRows */}
             </div>
         </div>
