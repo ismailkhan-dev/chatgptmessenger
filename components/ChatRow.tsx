96df9fe83e619da87499a844150b0c6c6d0fea96
viz1er <viz1er@yopmail.com>
2023-10-07 19:52:23 -0400

    add remove chat feature

--- components/ChatRow.tsx
+++ components/ChatRow.tsx
@@ -1,6 +1,6 @@
 import { db } from "../firebase";
 import { ChatBubbleLeftIcon, TrashIcon } from "@heroicons/react/24/outline";
-import { collection, orderBy, query } from "firebase/firestore";
+import { collection, deleteDoc, doc, orderBy, query } from "firebase/firestore";
 import { truncate } from "fs";
 import { useSession } from "next-auth/react";
 import Link from "next/link";
@@ -28,6 +28,11 @@ function ChatRow({ id }: Props) {
         setActive(pathname.includes(id));
     }, [pathname]);
 
+    const removeChat = async () => {
+        await deleteDoc(doc(db, "users", session?.user?.email!, "chats", id));
+        router.replace("/");
+    };
+
     return (
         <Link
             href={`/chat/${id}`}
@@ -38,7 +43,10 @@ function ChatRow({ id }: Props) {
                 {messages?.docs[messages?.docs.length - 1]?.data().text ||
                     "New Chat"}
             </p>
-            <TrashIcon className="h-5 w-5 text-gray-700 hover:text-red-700" />
+            <TrashIcon
+                onClick={removeChat}
+                className="h-5 w-5 text-gray-700 hover:text-red-700"
+            />
         </Link>
     );
 }
