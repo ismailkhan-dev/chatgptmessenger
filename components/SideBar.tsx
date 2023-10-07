734e9c2bde4f6f049cfe192f4c8753be34a37995
viz1er <viz1er@yopmail.com>
2023-10-07 14:12:15 -0400

    add chatrow and firebase hooks to newChat

--- components/SideBar.tsx
+++ components/SideBar.tsx
@@ -3,9 +3,18 @@
 import React from "react";
 import NewChat from "./NewChat";
 import { useSession, signOut } from "next-auth/react";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { collection } from "firebase/firestore";
+import { db } from "../firebase";
 
 function SideBar() {
     const { data: session } = useSession();
+
+    const [chats, loading, error] = useCollection(
+        session && collection(db, "users", session?.user?.email!, "chats")
+    );
+
+    console.log(chats);
 
     return (
         <div className="p-2 flex flex-col h-screen ">
@@ -14,6 +23,9 @@ function SideBar() {
                 <NewChat />
                 <div>{/* Model Selection */}</div>
                 {/* Map through the ChatRows */}
+                {chats?.docs.map((chat) => (
+                  <ChatRow key={chat.id} id={chat.id} />
+                )}
             </div>
 
             {session && (
