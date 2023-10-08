064e8117009fb6f7c95d12dd9ed191e9515fd76e
viz1er <viz1er@yopmail.com>
2023-10-07 23:13:59 -0400

    add chatinput feature

--- app/chat/[id]/page.tsx
+++ app/chat/[id]/page.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import Chat from "@/components/Chat";
+import ChatInput from "@/components/ChatInput";
 
-function ChatPage() {
-    return <div>ChatPage</div>;
+type Props = {
+    params: {
+        id: string;
+    };
+};
+function ChatPage({ params: { id } }: Props) {
+    console.log(id);
+
+    return (
+        <div className="flex flex-col h-screen overflow-hidden">
+            <Chat chatId={id} />
+            <ChatInput chatId={id} />
+        </div>
+    );
 }
 
 export default ChatPage;
